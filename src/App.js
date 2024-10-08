2f72c52d9d405f6e0cce4cc9d8abe6b8b530f9e9
kavyam1820@gmail.com <kavyam1820@gmail.com>
2024-10-08 23:12:14 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { Route, Routes } from 'react-router-dom';
+import Signup from './components/Signup';
+import Login from './components/login'; // Ensure case matches the file name
+import StudentDashboard from './components/StudentDashboard';
+import FacultyDashboard from './components/FacultyDashboard';
+import EditAssignment from './components/EditAssignment';
+import EditGivenAssignment from './components/EditGivenAssignment';
+import Grades from './components/Grades';
+import Feedback from './components/FacultyFeedback';
+import StudentFeedback from './components/StudentFeedback';
+import ViewFacultyFeedback from './components/ViewFacultyFeedback';
+import SubmitAssignment from './components/SubmitAssignment';
+import SubmissionCount from './components/SubmissionCount';
+import ViewStudentFeedback from './components/ViewStudentFeedback';
+import GiveAssignment from './components/GiveAssignment';
+import ViewAssignments from './components/ViewAssignments';
+import ViewSubmissions from './components/ViewSubmissions';
+
+
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <Routes>
+            <Route path="/" element={<Signup />} /> {/* Default route to show signup */}
+            <Route path="/login" element={<Login />} />
+            <Route path="/StudentDashboard" element={<StudentDashboard />} />
+            <Route path="/FacultyDashboard" element={<FacultyDashboard />} />
+            <Route path="/EditAssignment" element={<EditAssignment />} />
+            <Route path="/EditGivenAssignment" element={<EditGivenAssignment />} />
+            <Route path="/Grades" element={<Grades />} />
+            <Route path="/FacultyFeedback" element={<Feedback />} />
+            <Route path="/StudentFeedback" element={<StudentFeedback />} />
+            <Route path="/ViewFacultyFeedback" element={<ViewFacultyFeedback />} />
+            <Route path="/SubmitAssignment" element={<SubmitAssignment />} />
+            <Route path="/SubmissionCount" element={<SubmissionCount />} />
+            <Route path="/ViewStudentFeedback" element={<ViewStudentFeedback />} />
+            <Route path="/GiveAssignment" element={<GiveAssignment />} />
+            <Route path="/ViewSubmissions" element={<ViewSubmissions />} />
+            <Route path="/ViewAssignments" element={<ViewAssignments />} />
+            
+        </Routes>
+    );
 }
 
 export default App;
